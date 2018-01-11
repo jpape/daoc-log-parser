35918e28c24a310108dbd6d37e984a59e22fd811
nativex-robert.pape <robert.pape@nativex.com>
2018-01-11 15:14:31 -0600

    alt_parse fix

--- alt_parse.py
+++ alt_parse.py
@@ -511,13 +511,13 @@ def parse_test_file():
         print("Failed to open file")
         exit(0)
 
-def parse_uploaded_file(readf):
+def parse_uploaded_file(file):
     result = {}
     try:
-        # with open(file, 'r') as readf:
-        cash_flow = parse_cash_flow(readf)
-        combat = parse_combat(readf)
-        healing = parse_healing(readf)
+        with open(file, 'r') as readf:
+            cash_flow = parse_cash_flow(readf)
+            combat = parse_combat(readf)
+            healing = parse_healing(readf)
 
             # print_money(cash_flow)
             # print_combat(combat)
