12e39ffb06a1b06d6e061e4c61b5f3906acdcee8
nativex-robert.pape <robert.pape@nativex.com>
2018-01-11 14:30:27 -0600

    local py

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -294,7 +294,8 @@ var ParserService = (function () {
         // optHeaders.append('Content-Type', 'multipart/form-data');
         optHeaders.append('Accept', 'application/json');
         var options = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["c" /* RequestOptions */]({ headers: optHeaders });
-        return this.httpService.post('https://daoc-log-parse.herokuapp.com/upload', formData, options)
+        // return this.httpService.post('https://daoc-log-parse.herokuapp.com/upload', formData, options)
+        return this.httpService.post('https://localhost:5000/upload', formData, options)
             .map(function (response) {
             var res = response.json();
             return res;
--- src/app/services/parser.service.ts
+++ src/app/services/parser.service.ts
@@ -19,7 +19,8 @@ export class ParserService {
     // optHeaders.append('Content-Type', 'multipart/form-data');
     optHeaders.append('Accept', 'application/json');
     let options = new RequestOptions({ headers: optHeaders});
-    return this.httpService.post('https://daoc-log-parse.herokuapp.com/upload', formData, options)
+    // return this.httpService.post('https://daoc-log-parse.herokuapp.com/upload', formData, options)
+    return this.httpService.post('https://localhost:5000/upload', formData, options)
       .map((response: Response) => {
         let res = response.json();
         return res;
